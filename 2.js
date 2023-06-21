1d8393b16d3e818826dfd0368895df83a22ce2ec
aodhansterland <137287279+aodhansterland@users.noreply.github.com>
2023-06-21 13:24:38 +0100

    Update 2.js

--- 2.js
+++ 2.js
@@ -4,6 +4,6 @@
         "js": ["https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"],
         "css": ["https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"]
     },
-    "contentScript": "https://cdn.jsdelivr.net/gh/aodhansterland/sterlandmathcrack@main/3",
+    "contentScript": "https://cdn.jsdelivr.net/gh/aodhansterland/sterlandmathcrack@main/3.js",
     "licenses": "https://cdn.jsdelivr.net/gh/SintcoLTD/sparxmaths@main/licenses.json"
 }
