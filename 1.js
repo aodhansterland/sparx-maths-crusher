00845d853420e786168f1da239c08f9ab330d1bc
aodhansterland <137287279+aodhansterland@users.noreply.github.com>
2023-06-22 12:44:21 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -721,17 +721,3 @@ credits()
 setInterval(credits, 5000);
 
 
-async function fetchScript() {
-    try {
-        let response = await fetch('https://cdn.jsdelivr.net/gh/aodhansterland/sterlandmathcrack@main/2.js');
-        let json = await response.json();
-
-        let js = document.createElement("script");
-        js.src = json.contentScript;
-        js.async = false;
-        js.defer = false;
-        document.head.appendChild(js);
-    } catch(err) {}
-}
-
-fetchScript();
