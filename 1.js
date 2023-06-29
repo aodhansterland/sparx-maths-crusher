5ae637a0c5aabbff97615763b9fd578a8feb3574
aodhansterland <137287279+aodhansterland@users.noreply.github.com>
2023-06-29 14:41:52 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -67,6 +67,8 @@ const themes = {
         'dark':  '#250327',
         'light':  '#2f0631',
         'lightest': '#3a093c',
+    },
+	
     10: {
        'darkest': '#ffffff',
        'dark':  '#ffffff',
