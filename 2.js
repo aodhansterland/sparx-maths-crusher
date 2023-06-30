c2dec1fb78c0f3fa6c585bf095dbb6720d453b35
aodhansterland <137287279+aodhansterland@users.noreply.github.com>
2023-06-30 14:08:39 +0100

    Delete 2.js

--- 2.js
+++
@@ -1,9 +0,0 @@
-{
-    "version": "v3.1.0",
-    "dependencies": {
-        "js": ["https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"],
-        "css": ["https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"]
-    },
-    "contentScript": "https://cdn.jsdelivr.net/gh/aodhansterland/sterlandmathcrack@main/3.js",
-    "licenses": "https://cdn.jsdelivr.net/gh/SintcoLTD/sparxmaths@main/licenses.json"
-}
